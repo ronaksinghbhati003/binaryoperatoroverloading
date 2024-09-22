aa23eaddba195f2554d12724211d45b775ea4ce1
ronaksinghbhati003 <singhbhatironak2004@gmail.com>
2024-09-22 20:36:57 +0530

    Add files via upload

---
+++ Operator_overloading_addtwocomplex.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class complex{
+	private :
+		int x,y;
+	public :
+		complex(){};
+		complex(int real,int img)
+		{
+			x=real;
+			y=img;
+		}
+		complex operator+(complex c)
+		{
+			complex temp;
+			temp.x=x+c.
+		}
+};
